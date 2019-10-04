307877f884aea4cfeccf41f3c2e4e6e385e40b86
Rogerio <rogeri_22@hotmail.com>
2019-10-04 20:44:41 -0300

    Exercício 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComandaComponent } from './comanda/comanda.component';
@@ -12,7 +12,8 @@ import { ComandaComponent } from './comanda/comanda.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
